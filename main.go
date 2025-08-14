bd4cb784e7c4605526606137ba1c7eb04dd27a34
Ruslan <r_mukhamedzhanov@bk.ru>
2025-08-14 20:04:39 +0500

    Done

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func main() {
+	// Инициализация генератора случайных чисел (обязательно!)
+	rand.Seed(time.Now().UnixNano())
+
+	http.HandleFunc("/rand", Number)
+	http.ListenAndServe(":9091", nil)
+}
+
+func Number(w http.ResponseWriter, r *http.Request) {
+	randomnumber := rand.Intn(7) // Генерируем число от 0 до 6
+
+	// Преобразуем число в строку и отправляем клиенту
+	w.Write([]byte(strconv.Itoa(randomnumber)))
+
+	// Дополнительно выводим в консоль (необязательно)
+	fmt.Println("Сгенерировано:", randomnumber)
+
+}
