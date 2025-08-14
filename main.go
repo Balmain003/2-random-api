5abe1f724b722bed4d72909ec404a9b48d1a4ced
Ruslan <r_mukhamedzhanov@bk.ru>
2025-08-14 20:32:43 +0500

    Done

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 }
 
 func Number(w http.ResponseWriter, r *http.Request) {
-	randomnumber := rand.Intn(7) // Генерируем число от 0 до 6
+	randomnumber := rand.Intn(6) + 1 // Генерируем число от 1 до 6
 
 	// Преобразуем число в строку и отправляем клиенту
 	w.Write([]byte(strconv.Itoa(randomnumber)))
